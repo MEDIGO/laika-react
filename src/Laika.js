2719f2678d923c0404a23dbfb2479dda7d697b78
peterzernia <peter.zernia@medigo.com>
2019-08-30 12:48:31 +0200

    moved utils into main js

--- src/Laika.js
+++ src/Laika.js
@@ -2,6 +2,30 @@ import React, { useState, useEffect } from 'react'
 import { shape, string } from 'prop-types'
 import { retrieve } from './utils'
 
+const DEFAULT_TIMEOUT = 60 * 60 * 1000
+
+export const timeoutKey = (key) => `${key}_timeout`
+
+export function store(key, value) {
+  localStorage.setItem(key, JSON.stringify(value))
+  localStorage.setItem(timeoutKey(key), Date.now())
+  return value
+}
+
+export function retrieve(key, retrievalFunction, timeout = DEFAULT_TIMEOUT) {
+  const ts = localStorage.getItem(timeoutKey(key)) || new Date('1970-1-1').getTime()
+
+  if (ts && (Date.now() - ts) > timeout) {
+    return retrievalFunction()
+      .then((response) => store(key, response))
+      .catch(() => {
+      // fail silently
+      })
+  }
+  return JSON.parse(localStorage.getItem(key))
+}
+
+
 export default function Laika({ url, env, feature, onTrue, onFalse }) {
   const [status, setStatus] = useState(false)
 
