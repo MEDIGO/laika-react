eec102d7a9541286e6496f44a4ee8f377bb5f283
peterzernia <peter.zernia@medigo.com>
2019-08-30 13:50:10 +0200

    added demo

---
+++ demo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Laika from 'laika-react';
+
+render(
+  <div>
+    <h2>Laika React Component Demo</h2>
+    <Laika
+      url=""
+      feature=""
+      env=""
+      onTrue={<div style={{ width: 50, height: 50, backgroundColor: 'red' }} />}
+      onFalse={<div style={{ width: 50, height: 50, backgroundColor: 'blue' }} />}
+    />
+  </div>, 
+  document.getElementById('app')
+);
